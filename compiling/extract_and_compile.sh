df6f1a50b0e65974629fc23caee113a3ae2389c0
Nicholas Gilbertson <gilb0578@x-146-57-33-157.mrs.umn.edu>
2021-09-02 10:11:51 -0500

    Fix version number for automated testing

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -5,5 +5,5 @@ extract_and_compile() {
 	cd NthPrime || exit #mostly to satisfy shellcheck since we know it'll exist since we just made it
 	gcc -- *.c *.h  -o NthPrime #compiles all of the files together into something named NthPrime.
 	#-- is partially for shellcheck, but also nice if any files start with -.
-	./NthPrime "$1" #quotes are mostly to satisfy shellcheck since we know arguments will be safe numbers
+	./NthPrime "$1" #quotes are mostly to satisfy shellcheck since we know arguments will be safe 
 }
