e6bec3316d4e070a62b9f9a6c644238bdc32daf0
Nicholas Gilbertson <gilb0578@sentinel.morris.umn.edu>
2021-09-07 10:12:28 -0500

    Fixed syntax error in compiling code

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-extract_and_compile() {
-	tar NthPrime.tgz -z -x #unzip and extract all in one
-	cd NthPrime || exit #mostly to satisfy shellcheck since we know it'll exist since we just made it
-	gcc -- *.c *.h  -o NthPrime #compiles all of the files together into something named NthPrime.
-	#-- is partially for shellcheck, but also nice if any files start with -.
-	./NthPrime "$1" #quotes are mostly to satisfy shellcheck since we know arguments will be safe 
-}
+tar -zxf NthPrime.tgz #unzip and extract all in one
+cd NthPrime || exit #mostly to satisfy shellcheck since we know it'll exist since we just made it
+gcc *.c *.h  -o NthPrime #compiles all of the files together into something named NthPrime.
+./NthPrime "$1" #quotes are mostly to satisfy shellcheck since we know arguments will be safe 
