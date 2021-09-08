704a869e6f5f0f52c8eda2fb788dbf3d806863d7
Nicholas Gilbertson <gilb0578@sentinel.morris.umn.edu>
2021-09-08 15:43:23 -0500

    Progress: zipped files but with wrong names

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-StartLoc="$(realpath "$1")"/".." #stores where we started at
+StartLoc="$(realpath "$1")" #stores where we started at
 TempName=$(mktemp -d) #Stores the name of the temporary directory by piping to mktemp
-tar -zxf "$1" "$TempName"  #unzips, extracts, and then specifies where
-cd "$TempName" || exit
+tar -C "$TempName" -zxf "$StartLoc" #unzips, extracts, and then specifies where
+#cd "$TempName" || exit
 cleaned_name="cleaned_$1"
+grep -Lr "DELETE ME!" "$TempName" | xargs tar -cf "$cleaned_name"
+
 #grep -Le="DELETE ME!" | xargs tar -c -f="$cleaned_name" #take everything that doesn't have the word, and tar it
-grep -le "DELETE ME!" | xargs rm
-cd "$StartLoc"
-tar -czf "$cleaned_name" *
+#grep -le "DELETE ME!" | xargs rm
+#cd "$StartLoc"
+#tar -czf "$cleaned_name" *
+
+
 rm -r "$TempName"
