6674cbe49d393ae0a3a3c0e119b429953689c347
Nicholas Gilbertson <gilb0578@sentinel.morris.umn.edu>
2021-09-08 15:14:00 -0500

    Troubleshooting Progress

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-clean (){
-	TempName=$(mktemp -d) #Stores the name of the temporary directory by piping to mktemp
-	tar "$1" -z -x -f "$TempName" #unzips, extracts, and then specifies where
-	cd "$TempName" || exit
-	cleaned_name="cleaned_$1"
-	grep -L -f=* -e="DELETE ME!" | xargs tar -c -f="$cleaned_name" #take everything that doesn't have the word, and tar it
-	rm -rf "$TempName"
-}	
+
+StartLoc="$(realpath "$1")"/".." #stores where we started at
+TempName=$(mktemp -d) #Stores the name of the temporary directory by piping to mktemp
+tar -zxf "$1" "$TempName"  #unzips, extracts, and then specifies where
+cd "$TempName" || exit
+cleaned_name="cleaned_$1"
+#grep -Le="DELETE ME!" | xargs tar -c -f="$cleaned_name" #take everything that doesn't have the word, and tar it
+grep -le "DELETE ME!" | xargs rm
+cd "$StartLoc"
+tar -czf "$cleaned_name" *
+rm -r "$TempName"
