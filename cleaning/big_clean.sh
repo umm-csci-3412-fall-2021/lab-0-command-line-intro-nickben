30960ef9a039b19599e5f86a980a68d8479606ad
Nicholas Gilbertson <gilb0578@rynite.morris.umn.edu>
2021-09-08 19:30:59 -0500

    cleaning up the code and fixing shellcheck

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -3,19 +3,9 @@
 StartLoc="$(pwd)" #stores where we started at
 TempName=$(mktemp -d) #Stores the name of the temporary directory by piping to mktemp
 tar -zxf "$1" -C "$TempName" #unzips, extracts, and then specifies where
-#cd "$TempName" || exit
 cleaned_name="cleaned_$1"
-#grep -Lr "DELETE ME!" | xargs basename -a | xargs tar -zhcf "$StartLoc/$cleaned_name" #Find everything not flagged to be deleted, and archive it
-grep -lr "DELETE ME!" "$TempName" | xargs rm
-tar -C "$TempName" -zhcf "$StartLoc/$cleaned_name" "$(basename "$1" .tgz)"
-
-#mv "$StartLoc" "$cleaned_name" #Move the archive back to where it belongs
+grep -lr "DELETE ME!" "$TempName" | xargs rm #Delete every file which contains the phrase "DELETE ME!"
+tar -C "$TempName" -zhcf "$StartLoc/$cleaned_name" "$(basename "$1" .tgz)" #From the temp location, since we know what the inner folder will be,
+#and we know we've cleaned out everything that needs to be deleted, we simply archive the folder and everything left in it.
 cd "$StartLoc" || exit #go back to where we started, for simplicity
-
-#grep -Le="DELETE ME!" | xargs tar -c -f="$cleaned_name" #take everything that doesn't have the word, and tar it
-#grep -le "DELETE ME!" | xargs rm
-#cd "$StartLoc"
-#tar -czf "$cleaned_name" *
-
-
 rm -r "$TempName"
--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -2,5 +2,5 @@
 
 tar -zxf NthPrime.tgz #unzip and extract all in one
 cd NthPrime || exit #mostly to satisfy shellcheck since we know it'll exist since we just made it
-gcc *.c *.h  -o NthPrime #compiles all of the files together into something named NthPrime.
+gcc ./*.c ./*.h  -o NthPrime #compiles all of the files together into something named NthPrime.
 ./NthPrime "$1" #quotes are mostly to satisfy shellcheck since we know arguments will be safe 
